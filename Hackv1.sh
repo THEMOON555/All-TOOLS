196b8b256e5e9a642b2f8c90b6f67ef5a0271c2e
TheDemon <108716811+THEMOON555@users.noreply.github.com>
2022-07-11 23:05:36 +0800

    Create Hackv1.sh

---
+++ Hackv1.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+#warna                                                                                              >
+merah="\033[31m"
+hijau="\033[32m"
+kuning="\033[1;33m"
+biru="\033[34m"
+merahmuda="\33[35m"
+birumuda="\33[1;36m"
+putih="\033[37m"
+
+
+
+menu() {
+clear
+   echo
+   echo -e "\033[31m$(figlet -f Remo773 "BIASA AJA")\e[0m"
+   echo
+   echo -e $merah" ╔═══════════════════════════════════════════════╗"
+   echo -e $kuning" ║"   "\033[1;34mAuthor    : DEMONXX                       "$putih"     ║"
+   echo -e $kuning" ║"   "\033[1;34mTeam      : Cyber Hunter Indonesia        "$putih"     ║"
+   echo -e $kuning" ║"   "\033[1;34mGithub    : https://github.com/THEMOON555 "$putih"     ║"
+   echo -e $putih" ╚═══════════════════════════════════════════════╝"
+
+   echo -e $putih "╔════════════════════════╗"
+   echo -e " ║ " "\033[1;35m  Tools XXXXXXXXXX" $putih "  ║"
+   echo -e $putih "╚════════════════════════╝"
+   echo
+   echo -e $putih "[1]""  Versi-1" $hijau "   [ON]"
+   echo -e $putih "[2]""  Versi-2" $hijau "    [ON]"
+   echo -e $putih "[3]""  Versi-3" $hijau "     [ON]"
+   echo -e $putih "[4]" $merah" Keluar/Exit"
+   echo
+   mainmenu
+}
+
+mainmenu() {
+    echo -e -n "\033[1;31mPilih ngab = ";read pil
+    if [ $pil == "1" ];then
+    git clone https://github.com/THEMOON555/Haaa
+    cd Haa
+    npm i
+    node index
+    echo "[√] DONE>.<"
+    elif [ $pil == "2" ];then
+    git clone https://github.com/THEMOON555/XD-LOM
+    cd XD-LOM
+    npm i
+    node index
+    echo "[√] DONE>.<"
+    elif [ $pil == "3" ];then
+    git clone https://github.com/THEMOON555/yaaa
+    cd yaaa
+    npm i
+    node index
+    echo "[√] DONE>.<"
+    elif [ $pil == "4" ];then
+    exit
+    else
+    echo "[!] enter the correct number dog!!!"
+    fi
+}
+menu
